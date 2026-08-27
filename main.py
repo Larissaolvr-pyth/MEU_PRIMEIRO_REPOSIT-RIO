0ceebe4b7ed05b84ad60bbb820fb94134db8b838
Larissaolvr <alane.larissa.oliveira@outlook.com>
2026-08-27 20:35:26 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+my_words = ["Hi!", "Python Pro", "Kodland"] 
+print(my_words[1])
+
+name = input()
+print("Hi ", name)
+
+about_me = ["Guitarra", "Python", "Matemática", "Spider-Man", "Rock"]
+print(random.choice(about_me))
